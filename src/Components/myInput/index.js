8ab1c5fe1785930f42225b83d728f75be35b8625
hu.noh <shd7561@gmail.com>
2025-02-20 10:46:48 +0900

    예외처리

--- src/Components/myInput/index.js
+++ src/Components/myInput/index.js
@@ -1,16 +1,16 @@
-const MyInput = ({ type, placeholder, value, onChange, onKeyUp }) => {
-  return (
-    <div>
-      <input
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        onKeyUp={onKeyUp}
-        onChange={onChange}
-        className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none text-gray-700 placeholder-gray-400 transition duration-300 ease-in-out"
-      />
-    </div>
-  );
+const MyInput = ({ type, placeholder, value, onChange, maxlength }) => {
+    return (
+        <div>
+            <input
+                type={type}
+                placeholder={placeholder}
+                value={value}
+                maxLength={maxlength}
+                onChange={onChange}
+                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none text-gray-700 placeholder-gray-400 transition duration-300 ease-in-out"
+            />
+        </div>
+    );
 };
 
 export default MyInput;
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -3,153 +3,182 @@ import MyInput from "../../Components/myInput";
 import MyButton from "../../Components/myButton";
 import CalcButton from "../../Components/calcButton";
 import ExpButton from "../../Components/expButton";
-import expList from "./../../data";
+import data from "./../../data";
 
-const timeList = [
-  { id: 5, name: "5분", value: 5 },
-  { id: 10, name: "10분", value: 10 },
-  { id: 30, name: "30분", value: 30 },
-  { id: 60, name: "1시간", value: 60 },
-];
-
-const addList = [
-  { id: "10000", name: "1만", value: "10000" },
-  { id: "50000", name: "5만", value: "50000" },
-  { id: "100000", name: "10만", value: "100000" },
-  { id: "500000", name: "50만", value: "500000" },
-  { id: "1000000", name: "100만", value: "1000000" },
-  { id: "5000000", name: "500만", value: "5000000" },
-];
 const Home = () => {
-  const [currentLvl, setCurrentLvl] = useState("");
-  const [targetLvl, setTargetLvl] = useState("");
-  const [currentExp, setCurrentExp] = useState("");
-  const [exp, setExp] = useState("");
+    const [currentLvl, setCurrentLvl] = useState("");
+    const [targetLvl, setTargetLvl] = useState("");
+    const [currentExp, setCurrentExp] = useState("");
+    const [exp, setExp] = useState("");
 
-  const [nowTime, setNowTime] = useState(5);
+    const [nowTime, setNowTime] = useState(5);
 
-  const [needTime, setNeedTime] = useState(0);
+    const [needTime, setNeedTime] = useState(0);
 
-  const onKeyUp = (e) => {
-    e.target.value.replace(/[^0-9]/g, "");
-  };
-  const onChangeLvl = (e) => {
-    const newValue = e.target.value;
-    setCurrentLvl(newValue);
-  };
+    // 에러메시지
+    const [errorMsg, setErrorMsg] = useState("");
 
-  const onChangeTargetLvl = (e) => {
-    const newValue = e.target.value;
-    setTargetLvl(newValue);
-  };
+    const onChangeLvl = (e) => {
+        const newValue = e.target.value;
+        setCurrentLvl(newValue);
+    };
 
-  const onChangeCurrentExp = (e) => {
-    const newValue = e.target.value;
+    const onChangeTargetLvl = (e) => {
+        const newValue = e.target.value;
+        setTargetLvl(newValue);
+    };
 
-    setCurrentExp(newValue);
-  };
+    const onChangeCurrentExp = (e) => {
+        const newValue = e.target.value;
 
-  const onChangeExp = (e) => {
-    const newValue = e.target.value;
+        setCurrentExp(newValue);
+    };
 
-    setExp(newValue);
-  };
+    const onChangeExp = (e) => {
+        const newValue = e.target.value;
 
-  const addExp = (addValue) => {
-    const newValue = Number(exp) + Number(addValue);
-    setExp(newValue);
-  };
+        setExp(newValue);
+    };
 
-  // 초기화
-  const onCalcInit = () => {
-    setExp("");
-    setNeedTime("");
-  };
+    const addExp = (addValue) => {
+        const newValue = Number(exp) + Number(addValue);
+        setExp(newValue);
+    };
 
-  const onCalcExp = () => {
-    if (!currentLvl || !targetLvl || !exp) {
-      return;
-    }
+    // 초기화
+    const onCalcInit = () => {
+        setExp("");
+        setNeedTime(0);
+    };
 
-    let needExp = 0;
+    // 계산하기
+    const onCalcExp = () => {
+        if (!currentLvl || !targetLvl || !exp) {
+            setErrorMsg("레벨과 획득 경험치를 입력해주세요.");
+            return;
+        }
 
-    for (let i = currentLvl; i < targetLvl; i++) {
-      needExp += Number(expList.exps[i]);
-    }
+        const numCurrentLvl = parseInt(currentLvl);
+        const numTargetLvl = parseInt(targetLvl);
 
-    needExp = currentExp ? needExp - Number(currentExp) : needExp;
-    console.log(needExp);
-    let resultTime = ((needExp / exp) * Number(nowTime)) / 60;
-    resultTime = Math.round(resultTime * 1000) / 1000;
-    setNeedTime(resultTime);
-  };
+        if (
+            numCurrentLvl > 199 ||
+            numCurrentLvl < 1 ||
+            numTargetLvl > 200 ||
+            numTargetLvl < 2 ||
+            numTargetLvl <= numCurrentLvl
+        ) {
+            setErrorMsg("레벨을 재설정해주세요.");
+            return;
+        }
 
-  return (
-    <div className="p-10 sm:w-2/3 md:w-2/3 lg:w-1/3 w-2/3 space-y-3">
-      <div className="grid gap-6 mb-6 grid-cols-2">
-        <div>
-          <MyInput
-            type="number"
-            value={currentLvl}
-            onChange={onChangeLvl}
-            onKeyUp={onKeyUp}
-            placeholder="현재 레벨"
-          />
+        setErrorMsg("");
+
+        let needExp = 0;
+
+        for (let i = currentLvl; i < targetLvl; i++) {
+            needExp += Number(data.exps[i]);
+        }
+
+        needExp = currentExp ? needExp - Number(currentExp) : needExp;
+
+        let resultTime = ((needExp / exp) * Number(nowTime)) / 60;
+        resultTime = Math.round(resultTime * 1000) / 1000;
+        setNeedTime(resultTime);
+    };
+
+    return (
+        <div className="p-20 sm:w-2/3 md:w-2/3 lg:w-1/2 xl:w-1/2 space-y-5">
+            <div className="text-left space-y-1">
+                <p className="text-xl font-bold">메이플랜드 경험치 계산기</p>
+                <p className="text-sm text-gray-400">
+                    레벨업 예상시간을 확인해보세요!
+                </p>
+            </div>
+            <div className="space-y-3">
+                <div className="grid gap-6 grid-cols-2">
+                    <div>
+                        <MyInput
+                            type="text"
+                            value={currentLvl}
+                            maxlength={3}
+                            onChange={onChangeLvl}
+                            placeholder="현재 레벨"
+                        />
+                    </div>
+                    <div>
+                        <MyInput
+                            type="text"
+                            value={targetLvl}
+                            maxlength={3}
+                            onChange={onChangeTargetLvl}
+                            placeholder="목표 레벨"
+                        />
+                    </div>
+                </div>
+                <MyInput
+                    id="3"
+                    type="text"
+                    onChange={onChangeCurrentExp}
+                    maxlength={12}
+                    value={currentExp}
+                    placeholder="현재 경험치"
+                ></MyInput>
+            </div>
+            <div className="space-y-3">
+                <div className="flex space-x-1">
+                    {data.timeList.map((time) => (
+                        <div key={time.id}>
+                            <MyButton
+                                active={nowTime === time.id}
+                                onClick={() => setNowTime(time.id)}
+                            >
+                                {time.name}
+                            </MyButton>
+                        </div>
+                    ))}
+                </div>
+
+                <MyInput
+                    id="3"
+                    type="text"
+                    onChange={onChangeExp}
+                    value={exp}
+                    maxlength={12}
+                    placeholder="획득 경험치"
+                ></MyInput>
+
+                <div className="flex space-x-1">
+                    {data.expAddList.map((add) => (
+                        <div key={add.id}>
+                            <ExpButton onClick={() => addExp(add.value)}>
+                                {add.name}
+                            </ExpButton>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className="flex space-x-1">
+                <CalcButton type="refresh" onClick={onCalcInit}>
+                    초기화
+                </CalcButton>
+                <CalcButton type="calc" onClick={onCalcExp}>
+                    계산하기
+                </CalcButton>
+            </div>
+            {errorMsg && (
+                <div>
+                    <p className="text-red-500">{errorMsg}</p>
+                </div>
+            )}
+            <div>목표레벨까지 필요한 시간은</div>
+            <div>
+                <p className="font-bold text-2xl text-blue-500">
+                    {needTime} 시간
+                </p>
+            </div>
         </div>
-        <div>
-          <MyInput
-            type="number"
-            value={targetLvl}
-            onChange={onChangeTargetLvl}
-            onKeyUp={onKeyUp}
-            placeholder="목표 레벨"
-          />
-        </div>
-      </div>
-      <MyInput
-        id="3"
-        type="number"
-        onChange={onChangeCurrentExp}
-        value={currentExp}
-        placeholder="현재 경험치"
-      ></MyInput>
-      <div className="flex space-x-1">
-        {timeList.map((time) => (
-          <div key={time.id}>
-            <MyButton
-              active={nowTime === time.id}
-              onClick={() => setNowTime(time.id)}
-            >
-              {time.name}
-            </MyButton>
-          </div>
-        ))}
-      </div>
-      <MyInput
-        id="3"
-        type="number"
-        onChange={onChangeExp}
-        value={exp}
-        placeholder="획득 경험치"
-      ></MyInput>
-      <div className="flex space-x-1">
-        {addList.map((add) => (
-          <div key={add.id}>
-            <ExpButton onClick={() => addExp(add.value)}>{add.name}</ExpButton>
-          </div>
-        ))}
-      </div>
-      <div className="flex space-x-1">
-        <CalcButton type="refresh" onClick={onCalcInit}>
-          초기화
-        </CalcButton>
-        <CalcButton type="calc" onClick={onCalcExp}>
-          계산하기
-        </CalcButton>
-      </div>
-      <div>목표레벨까지 필요한 시간은 {needTime} 시간</div>
-    </div>
-  );
+    );
 };
 
 export default Home;
