9841ec01c720f53c77a680f46d6d65104807429b
hu.noh <shd7561@gmail.com>
2025-02-21 15:10:42 +0900

    basename 수정

--- src/Router/index.js
+++ src/Router/index.js
@@ -5,7 +5,7 @@ import Home from "../Pages/Home";
 const Router = () => {
     return (
         <div className="flex justify-center min-h-screen bg-gray-50 ">
-            <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Home />} />
                 </Routes>
