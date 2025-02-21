f914406a6874af7468bf43e00d6c0ded2713d5bf
hu.noh <shd7561@gmail.com>
2025-02-21 14:14:05 +0900

    base url 설정

--- src/Router/index.js
+++ src/Router/index.js
@@ -5,7 +5,7 @@ import Home from "../Pages/Home";
 const Router = () => {
     return (
         <div className="flex justify-center min-h-screen bg-gray-50 ">
-            <BrowserRouter>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <Routes>
                     <Route path="/" element={<Home />} />
                 </Routes>
